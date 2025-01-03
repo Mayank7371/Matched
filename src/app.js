5f29f50e121dfbbcc8c487827923c4f5cdea4296
Mayank7371 <neprone.dev@gmail.com>
2025-01-03 16:28:19 +0530

    update app.js and package.json

--- src/app.js
+++ src/app.js
@@ -3,13 +3,15 @@ const { log } = require("node:console");
 const app = express();
 const port = 3000;
 
-app.use("/", (req, res) => {
-  res.send("hi there");
+app.get("/user", (req, res) => {
+  console.log(req.query);
+  // URL is // http:localhost:3000/user?userId=101&username=mayank
 });
-app.get("/getroute", (req, res) => {
-  res.send("GET request");
+app.get("/user/:userId/:name/:password", (req, res) => {
+  // URL is localhost:3000/user?userId=101&name=mayank&password=mayank@101
+  // these are called dynamic route handler
+  console.log(req.params);
 });
-
 app.post("/postroute", (req, res) => {
   res.send("POST request");
 });
@@ -17,3 +19,21 @@ app.post("/postroute", (req, res) => {
 app.listen(port, () => {
   console.log(`app is listening on port ${port}`);
 });
+
+// comments
+//  work for /abc /ac
+// app.get("/ab?c", (req, res) => {
+//   res.send("GET request");
+// });
+//  works for /ad /abcd
+// app.get("/a(bc)?d", (req, res) => {
+//   res.send("GET request");
+// });
+// work for /abc /abbbbbbc
+// app.get("/ab+c", (req, res) => {
+//   res.send("GET request");
+// });
+// work for /abhellotherecd
+// app.get("/ab*cd", (req, res) => {
+//   res.send("GET request");
+// });
