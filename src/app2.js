1628e8cd610cc7062005438bf83dda67a443e626
Mayank <166874794+Mayank7371@users.noreply.github.com>
2025-01-06 22:14:36 +0530

    Add routes

--- src/app2.js
+++ src/app2.js
@@ -1,10 +1,33 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-app.use(express.json()); // used for parsing json data
-app.get("", () => {});
-app.post("", () => {});
-app.delete("", () => {});
-app.listen(port, () => {
-  console.log(`app is runnung on port : ${port}`);
+const fs = require("node:fs");
+const https = require("https");
+
+console.log("Hello World");
+
+var a = 1078698;
+var b = 20986;
+
+// Synchronous
+fs.readFileSync("./file.txt", "utf8"); // 10 ms
+console.log("This will execute only after file read");
+
+https.get("https://dummyjson.com/products/1", (res) => {
+  console.log("Fetched Data Successfully");
 });
+
+setTimeout(() => {
+  console.log("setTimeout called after 5 seconds");
+}, 5000);
+
+// Async function
+fs.readFile("./file.txt", "utf8", (err, data) => {
+  console.log("File Data : ", data);
+});
+
+function multiplyFn(x, y) {
+  const result = a * b;
+  return result;
+}
+
+var c = multiplyFn(a, b);
+
+console.log("Multiplication result is : ", c);
