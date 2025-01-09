e54d9c99f6f0e6ffa955ab8f4e85d1145fd495eb
Mayank7371 <neprone.dev@gmail.com>
2025-01-09 20:21:47 +0530

    update app2.js

--- src/app2.js
+++ src/app2.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -15,38 +14,4 @@ app.use(
 );
 app.listen(port, () => {
   console.log(`app is runnung on port : ${port}`);
-=======
-const fs = require("node:fs");
-const https = require("https");
-
-console.log("Hello World");
-
-var a = 1078698;
-var b = 20986;
-
-// Synchronous
-fs.readFileSync("./file.txt", "utf8"); // 10 ms
-console.log("This will execute only after file read");
-
-https.get("https://dummyjson.com/products/1", (res) => {
-  console.log("Fetched Data Successfully");
->>>>>>> 0718c5eefa2e9e766cab7b4b1536fc573da3020b
 });
-
-setTimeout(() => {
-  console.log("setTimeout called after 5 seconds");
-}, 5000);
-
-// Async function
-fs.readFile("./file.txt", "utf8", (err, data) => {
-  console.log("File Data : ", data);
-});
-
-function multiplyFn(x, y) {
-  const result = a * b;
-  return result;
-}
-
-var c = multiplyFn(a, b);
-
-console.log("Multiplication result is : ", c);
